d22edd50df3f7ebd423b7081245d1e4d73b6861c
Igunnuoda Olorunleke <s1900550@edu.bc.fi>
2020-05-26 12:37:20 +0300

    update the home page

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import Navbar from './components/Navbar'
 import Login from './components/Login'
 import { Route, Switch } from 'react-router-dom'
 import NotFound from './components/NotFound'
-
+import About from './components/About'
+import Home from './components/Home'
 const App = () => {
   return (
     <div>
       <Navbar />
       <Switch>
-        <Route exact path="/" component={Animals} />
-        <Route exact path="/login" component={Login} />
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/about" component={About} />
+        <Route path="/animals" component={Animals} />
+
         <Route component={NotFound} />
       </Switch>
      
